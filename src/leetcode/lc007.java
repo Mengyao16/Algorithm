5fa20728a3264ba461dd78741c8791a24beb7190
Mengyao16 <1274357077@qq.com>
2022-08-11 23:37:25 +0800

    update

---
+++ src/leetcode/lc007.java
@@ -0,0 +1,17 @@
+package Algorithm.leetcode;
+
+public class lc007 {
+    public int reverse(int x) {
+        long res = 0;
+        while(x!=0){
+            res = res * 10;
+            res = res + x % 10;
+            x = x / 10;
+        }
+        if(res>=Integer.MAX_VALUE|| res<=Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)res;
+
+    }
+}
